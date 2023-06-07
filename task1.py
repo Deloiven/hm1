a4028c86f896fdebadfeed4082f911c612cc0682
Ilya Vlasuyk <vlasuyk.2015.1@mail.ru>
2023-06-07 20:20:00 +0300

    all

---
+++ task1.py
@@ -0,0 +1,12 @@
+# Нарисовать ёлочку
+count_row = int(input("Введите число рядов в треугольнике: "))
+count_star = 1
+counter = count_row 
+
+while count_star <= count_row*2:
+    spase = " "
+    star = "*"
+    print(spase*counter, star*count_star,end="")
+    print("")
+    count_star +=2
+    counter -= 1
